a4701afeb0b33001bd3e063dee3abe4bb8bb658c
Geng Shuang <qh997@hotmail.com>
2015-01-20 17:30:53 +0800

    fs-update support auto running.

--- fs-update.pl
+++ fs-update.pl
@@ -46,12 +46,12 @@ my $chk_act = {
 	Modify => {
 		base => -1,
 		diff => 2,
-		deed => 'cp -r \'DIFF\' \'BASE/../\'',
+		deed => 'mkdir -p \'BASE/../\'; cp -r \'DIFF\' \'BASE/../\'',
 	},
 	New => {
 		base => -1,
 		diff => 1,
-		deed => 'cp -r \'DIFF\' \'BASE/../\'',
+		deed => 'mkdir -p \'BASE/../\'; cp -r \'DIFF\' \'BASE/../\'',
 	},
 };
 
@@ -59,82 +59,166 @@ my $check = 0;
 my $file_7z = '';
 my $extract = 0;
 GetOptions (
-    'c|check' => \$check,
-    'f|file=s' => \$file_7z,
-    'x|extract' => \$extract,
+	'c|check' => \$check,
+	'f|file=s' => \$file_7z,
+	'x|extract' => \$extract,
 );
 
-my $check_ok = 'check_ok_TTAG.sh';
+`mkdir -p $log_root` if (! -d $log_root);
+my $prefix_chkok = 'check_ok';
+my $perfix_done = 'accomplished';
 
 (-e $fs_root) or die "Cannot found `$fs_root'.\n";
 (-e $work_root) or die "Cannot found `$work_root'.\n";
-(-e $file_7z) or die "Cannot found `$file_7z'.\n";
 
-if ($check) {
+my @file_7zs;
+if ($file_7z) {
+	push @file_7zs, $file_7z;
+}
+else {
+	push @file_7zs, get_7z_file($archive_root);
+}
+
+if (! @file_7zs) {
+	print "There is nothing to do.\n";
+	print "Or you can specify a file manually:\n";
+	print "    `$0 -f example.7z -c -x'\n";
+	exit;
+}
+
+my $still_w = 0;
+my @act_files;
+foreach $file_7z (@file_7zs) {
+	my $check_ok = $log_root."/${prefix_chkok}_TTAG.sh";
 	my $unpack_path = $file_7z;
 	$unpack_path =~ s/^.*\///;
+	print "Working on `$unpack_path'\n";
 	$unpack_path =~ s/\.7z$//;
 	my $time_tag = $unpack_path;
 	$unpack_path = "$work_root/$unpack_path";
 	$time_tag =~ s/.*-//;
-	my $filelist_csv = "$unpack_path/filelist-${time_tag}.csv";
 	$check_ok =~ s/TTAG/$time_tag/;
+	my $err_suffix = '.need_fix';
 
-	`rm -rf $check_ok*`;
+	if ($check) {
+		my $filelist_csv = "$unpack_path/filelist-${time_tag}.csv";
 
-	unpack_7z($file_7z, $unpack_path) if ($extract);
+		`rm -rf $check_ok*`;
 
-	print 'Parsing csv ...';
-	my %types;
-	my $csv = Text::CSV->new({binary => 1})  # should set binary attribute.
-		or die "Cannot use CSV: ".Text::CSV->error_diag();
+		unpack_7z($file_7z, $unpack_path) if ($extract);
 
-	open my $fh, "<:encoding(utf8)", "$filelist_csv" or die "$filelist_csv: $!";
-	my $row_nu = 0;
-	while (my $row = $csv->getline($fh)) {
-		if ($row_nu++) {
-			my $type = $row->[2];
-			# next if $type eq 'None';
-			# next if $type eq 'Del';
-			# next if $type eq 'Modify';
-			# next if $type eq 'New';
-			if (!defined $types{$type}) {
-				$types{$type} = [];
+		print 'Parsing csv ...';
+		my %types;
+		my $csv = Text::CSV->new({binary => 1})  # should set binary attribute.
+			or die "Cannot use CSV: ".Text::CSV->error_diag();
+
+		open my $fh, "<:encoding(utf8)", "$filelist_csv" or die "$filelist_csv: $!";
+		my $row_nu = 0;
+		while (my $row = $csv->getline($fh)) {
+			if ($row_nu++) {
+				my $type = $row->[2];
+				# next if $type eq 'None';
+				# next if $type eq 'Del';
+				# next if $type eq 'Modify';
+				# next if $type eq 'New';
+				if (!defined $types{$type}) {
+					$types{$type} = [];
+				}
+
+				push @{$types{$type}}, [$row, $row_nu];
 			}
-
-			push @{$types{$type}}, [$row, $row_nu];
 		}
-	}
-	$csv->eof or $csv->error_diag();
-	close $fh;
-	$csv->eol("\r\n");
-	print "\n";
-
-	my @actions;
-	my $err_suffix = '';
-	foreach my $type (keys %types) {
-		print "Parsing $type...";
-		foreach my $row (@{$types{$type}}) {
-			my ($_rown, $_path, $_file) =
-				($row->[1], $row->[0]->[0], $row->[0]->[1]);
-			my $ret = item_check(\@actions, $fs_root, $unpack_path, 
-				$type, $row->[0]->[0], $row->[0]->[1]);
-			$err_suffix = '.need_fix' if $ret;
-			$actions[-1] .= " # $_rown($type) - $ret";
-		}
+		$csv->eof or $csv->error_diag();
+		close $fh;
+		$csv->eol("\r\n");
 		print "\n";
+
+		my @actions;
+		my $_err_suffix = '';
+		foreach my $type (keys %types) {
+			print "Parsing $type...";
+			foreach my $row (@{$types{$type}}) {
+				my ($_rown, $_path, $_file) =
+					($row->[1], $row->[0]->[0], $row->[0]->[1]);
+				my $ret = item_check(\@actions, $fs_root, $unpack_path, 
+					$type, $row->[0]->[0], $row->[0]->[1]);
+				$_err_suffix = $err_suffix if $ret;
+				$actions[-1] .= " # $_rown($type) - $ret";
+			}
+			print "\n";
+		}
+
+		open my $ofh, "> $check_ok$_err_suffix";
+		binmode($ofh, ':encoding(utf8)');
+		print $ofh "#!/bin/bash -ex\n";
+		print $ofh join "\n", @actions;
+		print $ofh "\n";
+		close $ofh;
+
+		if ($_err_suffix) {
+			print " - ********** WARNING **********\n";
+			print " - There are some warnings should be fixed.\n";
+			print " - Frist check the script `vim $check_ok$err_suffix'.\n";
+			print " -   Find the lines which start with `#W', and fix those warning lines.\n";
+			print " - Second `mv $check_ok$err_suffix $check_ok'\n";
+			print " - Then `$0'\n";
+		}
+	}
+	else {
+		if (-e "${check_ok}${err_suffix}") {
+			print "The warnings are already fixed?\n";
+			print "    `vim $check_ok$err_suffix'\n";
+			print "Or you just forget run this command:\n";
+			print "    `mv $check_ok$err_suffix $check_ok'\n";
+			$still_w = 1;
+		}
+		elsif (! -e $check_ok) {
+			print "Run `$0 --check' frist.\n";
+			$still_w = 1;
+		}
+		else {
+			push @act_files, $check_ok;
+		}
 	}
 
-	open my $ofh, "> $check_ok$err_suffix";
-	binmode($ofh, ':encoding(utf8)');
-	print $ofh "#!/bin/bash -ex\n";
-	print $ofh join "\n", @actions;
-	print $ofh "\n";
-	close $ofh;
+	print '=' x 57, "\n";
+}
+
+exit if $check;
+
+if ($still_w) {
+	print "\n";
+	print " ****************** FINAL WARNING ****************\n";
+	print " * There are still detected something abnormal.  *\n";
+	print " * I cannot let you go on.                       *\n";
+	print " * Please be sure to fix them.                   *\n";
+	print " *************************************************\n";
+	exit;
 }
 else {
-	if (! -e $check_ok) {
-		print "Run --check frist.\n";
+	print "OK, let's do it.\n";
+	foreach my $act_f (@act_files) {
+		chmod 0744, $act_f;
+
+		print "\n\$ $act_f\n";
+		my $ret_value = system("$act_f 2>&1 | tee cmd.log");
+		chmod 0644, $act_f;
+		my $ret = `cat cmd.log`;
+		`rm -rf cmd.log`;
+
+		system('sed', '-i', '1s/#!.*/# "DO NOT RUN THIS SCRIPT!!"/', "$act_f");
+		system('sed', '-i', '2iexit', "$act_f");
+
+		my $log_file = $act_f;
+		$log_file =~ s%${prefix_chkok}%${perfix_done}%;
+		$log_file =~ s%\.sh%.log%;
+		rename $act_f, $log_file;
+
+		open my $lfh, ">> $log_file";
+		print $lfh "\n", '#' x 27;
+		print $lfh " LOG ", '#' x 27, "\n";
+		print $lfh $ret;
+		close $lfh;
 	}
 }
 
@@ -230,7 +314,7 @@ sub item_check {
 		$action =~ s/DIFF/${diff_file}/g;
 		$action =~ s/BASE/${base_file}/g;
 		$action = @err ? "#W $action": $action;
-		$action =~ s%[^/]+/+\.\./+%%;
+		$action =~ s%[^/]+/+\.\./+%%g;
 
 		# print $action."\n";
 		push @$_actn, $action;
@@ -262,4 +346,32 @@ sub get_config {
 	}
 
 	return %configs;
+}
+
+sub get_7z_file {
+	my $arch_path = shift;
+
+	my @arch_list = `ls $archive_root/* | sort -r`;
+	if ($?) {
+		return ();
+	}
+
+	my @todo_list;
+	foreach my $_arch (@arch_list) {
+		chomp $_arch;
+		my $time_tag = $_arch;
+		$time_tag =~ s/.*-//;
+		$time_tag =~ s/\.7z$//;
+
+		if (-f "${log_root}/${perfix_done}_${time_tag}.log") {
+			last;
+		}
+		else {
+			print "To be update: $_arch\n";
+			unshift @todo_list, $_arch;
+		}
+	}
+
+	print '-' x 57 ,"\n";
+	return @todo_list;
 }
